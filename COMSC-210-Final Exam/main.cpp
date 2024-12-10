ad3f64577321f8faa281e27c7cf45aea6c5b6667
muster-Wugui <tintinpost28@gmail.com>
2024-12-09 18:45:27 -0800

    Initial Commit

--- COMSC-210-Final Exam/main.cpp
+++ COMSC-210-Final Exam/main.cpp
@@ -6,9 +6,30 @@
 //
 
 #include <iostream>
+#include <string>
+#include <ctime>
+#include <cstdlib>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+struct coffee {
+    string name;
+    string order;
+    coffee* next;
+    
+};
+
+int main() {
+    srand(static_cast<unsigned int>(time(0)));
+    
+    string names[10] = {"Austin", "Stepen", "Andrew", "Tony", "Bob", "James", "Smith", "Steve", "Amy", "Ruby"};
+    string drinks[10] = {"Latte", "Mocha", "Oatmilk", "Milk", "Coffee 1", "Coffee 2", "Coffee 3", "Coffee 4", "Coffee 5", "Lemonade"};
+    
+    coffee* head = nullptr;
+    coffee* tail = nullptr;
+    
+    for(int i = 0; i < 3; i++){
+        coffee* newNode = new coffee{names[reand() % 5], drinks}
+    }
+    
     return 0;
 }
