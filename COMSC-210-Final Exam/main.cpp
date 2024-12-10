1d789eb3fd6febb0cc2c685a65c2e65b8cf3a693
muster-Wugui <tintinpost28@gmail.com>
2024-12-09 19:20:15 -0800

    Fifth Commit

--- COMSC-210-Final Exam/main.cpp
+++ COMSC-210-Final Exam/main.cpp
@@ -11,6 +11,7 @@
 #include <cstdlib>
 #include <deque>
 #include <vector>
+#include <stack>
 using namespace std;
 
 struct coffee {
@@ -25,12 +26,13 @@ int main() {
     srand(static_cast<unsigned int>(time(0)));
     
     deque<pair<string, string>> muffins;
-    vector<pair<string, string>> bracelets;
+    vector<string> bracelets;
+    stack<pair<string, string>> phones;
     
     string names[10] = {"Austin", "Stepen", "Andrew", "Tony", "Bob", "James", "Smith", "Steve", "Amy", "Ruby"};
     string drinks[10] = {"Latte", "Mocha", "Oatmilk", "Milk", "Coffee 1", "Coffee 2", "Coffee 3", "Coffee 4", "Coffee 5", "Lemonade"};
     string muffin[10] = {"Chocaloate", "milk", "bluebarry", "banana", "muffin 1", "muffin 2", "muffin 3", "muffin 4", "muffin 5", "special flavor"};
-    
+    string phone[10] = {"Iphone 5", "Iphone 6", "Iphone 7", "Iphone 8", "Iphone 9", "Iphone 10", "Iphone 11", "Iphone 12", "Iphone 13", "Iphone 14"};
     
     coffee* head = nullptr;
     coffee* tail = nullptr;
@@ -71,12 +73,13 @@ int main() {
             
             cout<<"There is a new customer joined the line"<<endl;
         }
+        cout<<"-----------------------------------------------"<<endl;
         cout<<"Muffin Booth: "<<endl;
         if(muffins.empty()){
             cout<<"The line of muffin booth is empty."<<endl;
         }
         else{
-            cout<<"Now we are serving "<<muffins.front().first<<" who ordered a muffin"<<endl;
+            cout<<"Now we are serving "<<muffins.front().first<<" who ordered a "<<muffins.front().second<<" muffin"<<endl;
             muffins.pop_back();
         }
         
@@ -84,7 +87,23 @@ int main() {
             muffins.push_back(make_pair(names[rand() % 5], muffin[rand() % 5]));
             cout<<"There is a new customer joined the line"<<endl;
         }
-       
+        
+        cout<<"-----------------------------------------------"<<endl;
+        cout<<"Friendship Bracelets Booth: "<<endl;
+        if(bracelets.empty()){
+            cout<<"The line of friendship bracelets is empty."<<endl;
+        }
+        else{
+            cout<<"Now we are serving "<<bracelets[0]<<" who ordered a friendship bracelets"<<endl;
+            bracelets.erase();
+        }
+        
+        if(rand() % 2){
+            bracelets.push_back(names[rand() % 5]);
+            cout<<"There is a new customer joined the line"<<endl;
+        }
+        cout<<"-----------------------------------------------"<<endl;
+        cout<<"Phone Shop: "<<endl;
     }
     return 0;
 }
