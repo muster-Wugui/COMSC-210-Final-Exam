5f72db61fb22c4fb58ec3f57b8626f1794c728cf
muster-Wugui <tintinpost28@gmail.com>
2024-12-09 18:35:34 -0800

    Initial Commit

---
+++ COMSC-210-Final Exam/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  COMSC-210-Final Exam
+//
+//  Created by Xiao Zhang on 12/9/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
